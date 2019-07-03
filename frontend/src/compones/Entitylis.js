d021d918f7533113c8e47ce6ba14d2ab142a6c91
NahuelM <nahuelmartinez827@gmail.com>
2019-07-03 20:44:15 -0300

    frontend

---
+++ frontend/src/compones/Entitylis.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EntityList extends React.Component {
+    render() {
+      return (
+        <div className="EntityList">
+          Hola
+        </div>
+      );
+    }
+  }
+
+  export default EntityList
