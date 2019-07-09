ea1b9ff445725d09f9b3ec82f7f7a8560a5cdae3
NahuelM <nahuelmartinez827@gmail.com>
2019-07-08 22:03:54 -0300

    problemas con las base datos y dos carpetas q no puedo borrar

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 server = require("./server")
-
 mongoConnection = require("./src/mongo/mongoConnection")
 Home = require("./src/mongo/mongoHome")
+Farmacia = require("./src/farmacia")
+
 mongoConnection.connect( (db) => {
-    farmaciaHome = new Home("farmacia", db)    
+    farmaciaHome = new Home("Farmacia", db)    
+    folgera = new Farmacia("folgera")
+    farmaciaHome.insert(folgera)
     server.register(farmaciaHome)
+    server.register(folgera)
     server.init();
 })
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Redirect, NavLink} from "react-router-dom"
 import Entitylis from "./componentes/EntityList"
+
+function FarmaciasComponent(){
+  return (<Entitylis entity="farmacias"/>)
+}
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Entitylis> </Entitylis>
-        </p>
-    
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/Farmacias">Farmacias</NavLink></li>
+        </ul>
       </header>
+      <main className="App-main">
+          <Switch>
+            <Route path="/farmacias"  component={FarmaciasComponent} />
+            <Redirect to="/" />
+          </Switch>
+      </main>
+      </Router>
     </div>
   );
 }
